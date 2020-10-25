63793bbbbc1aa8d2ced510c1f75e2cedebe244b3
Michael Giddings <mikegiddings@gmail.com>
2020-10-25 16:23:01 -0400

    adde get employee by department. Added get all departments

--- models/employee.js
+++ models/employee.js
@@ -1,9 +1,8 @@
-const mysql = require('../db/dbconnect');
-
+const mysql = require("../db/dbconnect");
 
 const getAllEmployeesDetails = () => {
-    return new Promise((resolve, reject) => {
-      const query = `SELECT employee.id AS 'ID', 
+  return new Promise((resolve, reject) => {
+    const query = `SELECT employee.id AS 'ID', 
           first_name AS 'First Name', 
           last_name AS 'Last Name', 
           role.title AS 'Title', 
@@ -14,52 +13,73 @@ const getAllEmployeesDetails = () => {
         WHERE employee.role_id = role.id
           AND role.department_id = department.id
         ORDER BY employee.id ASC`;
-  
-      mysql.query(query, (err, results, fields) => {
-        if (err) {
-          console.log(err);
-          reject(err);
-        } else {
-          resolve(results);
-        }
-      });
+
+    mysql.query(query, (err, results, fields) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
+        resolve(results);
+      }
     });
-  };
+  });
+};
 
 const getAllEmployees = () => {
-    return new Promise((resolve, reject) => {
-      const query = 'SELECT * FROM employee';
-      mysql.query(query, (err, results, fields) => {
-        if (err) {
-          reject(err);
-        } else {
-          const employees = [];
-          for (const employee of results) {
-            const firstName = employee['first_name'];
-            const lastName = employee['last_name'];
-            employees.push(`${firstName} ${lastName}`);
-          }
-          resolve(employees);
+  return new Promise((resolve, reject) => {
+    const query = "SELECT * FROM employee";
+    mysql.query(query, (err, results, fields) => {
+      if (err) {
+        reject(err);
+      } else {
+        const employees = [];
+        for (const employee of results) {
+          const firstName = employee["first_name"];
+          const lastName = employee["last_name"];
+          employees.push(`${firstName} ${lastName}`);
         }
-      });
+        resolve(employees);
+      }
     });
-  };
-  const getManagerByID = managerID => {
-    return new Promise((resolve, reject) => {
-      const query = 'SELECT * FROM employee WHERE id = ?';
-      mysql.query(query, [managerID], (err, results, fields) => {
-        if (err) {
-          reject(err);
-        } else {
-          const manager = `${results[0]['first_name']} ${results[0]['last_name']}`;
-          resolve(manager);
-        }
-      });
-    });
-  };
+  });
+};
 
-  module.exports = {
-    getAllEmployees,
-    getAllEmployeesDetails,
-    getManagerByID
-  };
+const getManagerByID = (managerID) => {
+  return new Promise((resolve, reject) => {
+    const query = "SELECT * FROM employee WHERE id = ?";
+    mysql.query(query, [managerID], (err, results, fields) => {
+      if (err) {
+        reject(err);
+      } else {
+        const manager = `${results[0]["first_name"]} ${results[0]["last_name"]}`;
+        resolve(manager);
+      }
+    });
+  });
+};
+
+const getAllEmployeesByDepartment = (departmentID) => {
+  return new Promise((resolve, reject) => {
+    const query = `SELECT employee.id AS 'ID', 
+          first_name AS 'First Name', 
+          last_name AS 'Last Name'
+        FROM employee
+        WHERE employee.role_id = ANY (SELECT role.id FROM role WHERE role.department_id = ?)
+        ORDER BY employee.id ASC`;
+    mysql.query(query, [departmentID], (err, results, fields) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+};
+
+module.exports = {
+  getAllEmployees,
+  getAllEmployeesDetails,
+  getManagerByID,
+  getAllEmployeesByDepartment
+};
